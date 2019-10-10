07c1317ecdcfbed24cd701eb1286f53b5461895f
NatashaTihonova <36027415+NatashaTihonova@users.noreply.github.com>
2019-10-10 18:03:02 +0300

    QuickHull

---
+++ QuickHull/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace QuickHull
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.build = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // build
+            // 
+            this.build.Location = new System.Drawing.Point(646, 57);
+            this.build.Name = "build";
+            this.build.Size = new System.Drawing.Size(109, 46);
+            this.build.TabIndex = 1;
+            this.build.Text = "Построить оболочку";
+            this.build.UseVisualStyleBackColor = true;
+            this.build.Click += new System.EventHandler(this.build_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(646, 136);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(109, 46);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Очистить поле";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.build);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button build;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
